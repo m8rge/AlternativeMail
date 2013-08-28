33e4ce8b3fce3d5eb3dc89edcf74c71c4f482817
m8rge <to.merge@gmail.com>
2013-08-28 10:45:51 +0600

    full body test

--- tests/unit/BodyTest.php
+++ tests/unit/BodyTest.php
@@ -4,13 +4,18 @@ use m8rge\tests\TestCase;
 
 class BodyTest extends TestCase
 {
+    protected $textBody = 'I am text body';
+    protected $textEncoded = "SSBhbSB0ZXh0IGJvZHk=";
+
+    protected $htmlBody = '<hr />HTML IS AWESOME<hr />';
+    protected $htmlEncoded = "PGhyIC8+SFRNTCBJUyBBV0VTT01FPGhyIC8+";
+
     public function testTextMail()
     {
-        $textBody = 'I am text body';
-        list($to, $subject, $message, $additional_headers) = $this->mail->setTextBody($textBody)->send();
+        list($to, $subject, $message, $additional_headers) = $this->mail->setTextBody($this->textBody)->send();
         $this->assertEmpty($to);
         $this->assertEmpty($subject);
-        $this->assertEquals(base64_encode($textBody), $message);
+        $this->assertEquals($this->textEncoded, $message);
         $this->assertHeaders(
             array('Content-Type: text/plain; charset=UTF-8', 'Content-Transfer-Encoding: base64'),
             $additional_headers
@@ -19,11 +24,10 @@ class BodyTest extends TestCase
 
     public function testHtmlMail()
     {
-        $htmlBody = '<hr />HTML IS AWESOME<hr />';
-        list($to, $subject, $message, $additional_headers) = $this->mail->setHtmlBody($htmlBody)->send();
+        list($to, $subject, $message, $additional_headers) = $this->mail->setHtmlBody($this->htmlBody)->send();
         $this->assertEmpty($to);
         $this->assertEmpty($subject);
-        $this->assertEquals(base64_encode($htmlBody), $message);
+        $this->assertEquals($this->htmlEncoded, $message);
         $this->assertHeaders(
             array('Content-Type: text/html; charset=UTF-8', 'Content-Transfer-Encoding: base64'),
             $additional_headers
@@ -37,6 +41,27 @@ class BodyTest extends TestCase
         $this->assertEmpty($subject);
         $this->assertEmpty($message);
         $this->assertEmpty($additional_headers);
+    }
+
+    public function testMixedMail()
+    {
+        list($to, $subject, $message, $additional_headers) = $this->mail
+            ->setTextBody($this->textBody)
+            ->setHtmlBody($this->htmlBody)
+            ->send();
+        $this->assertEmpty($to);
+        $this->assertEmpty($subject);
+
+        $boundaryHeader = 'Content-Type: multipart/alternative; boundary=';
+        $this->assertStringStartsWith($boundaryHeader, $additional_headers);
+        $boundary = substr($additional_headers, strlen($boundaryHeader));
+
+        $this->assertStringStartsWith("--$boundary\r\n", $message);
+        $this->assertContains("--$boundary\r\nContent-Type: text/plain; charset=UTF-8\r\nContent-Transfer-Encoding: base64"
+            . "\r\n\r\n{$this->textEncoded}\r\n\r\n", $message);
+        $this->assertContains("--$boundary\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Transfer-Encoding: base64"
+            . "\r\n\r\n{$this->htmlEncoded}\r\n\r\n", $message);
+        $this->assertStringEndsWith("--$boundary--", $message);
     }
 
 }
